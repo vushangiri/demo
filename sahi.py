92f5626e63f0956730bb0dd4537a414fff05cc28
sijandh35 <sumoseason35@gmail.com>
2020-03-06 00:57:46 +0545

    Create sahi.py

---
+++ sahi.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb=mysql.connector.connect(host="localhost",user="Sijan",passwd="hangover35",database="Sijan")
+
+
+mycursor = mydb.cursor()
+
+mycursor.execute("select * from student")
+result=mycursor.fetchone()
+for i in mycursor:
+    print(i)
